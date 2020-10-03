2d73891aded547f88654b10076f51b3fad3b230e
jonay2000 <jonabent@gmail.com>
2020-10-03 17:13:06 +0200

    fixed clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
 #![no_std]
 
-
-#[cfg(not(feature="alloc"))]
+#[cfg(not(feature = "alloc"))]
 mod with_alloc;
-#[cfg(not(feature="alloc"))]
+#[cfg(not(feature = "alloc"))]
 pub use with_alloc::RingBuffer;
 
-
 mod with_generic_array;
+pub use generic_array::{typenum, ArrayLength};
 pub use with_generic_array::RingBuffer as GenericRingBuffer;
-pub use generic_array::{ArrayLength, typenum};
