8cdb084cf9b48fb88c9a497c3acf09b2ba473e9d
jonay2000 <jonabent@gmail.com>
2020-10-03 17:51:44 +0200

    Added const generic implementation of RingBuffer

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 #![no_std]
+#![cfg_attr(feature = "const_generics", feature(const_generics))]
+#![cfg_attr(feature = "const_generics", allow(incomplete_features))]
 
-#[cfg(not(feature = "alloc"))]
+#[cfg(feature = "alloc")]
 mod with_alloc;
-#[cfg(not(feature = "alloc"))]
+#[cfg(feature = "alloc")]
 pub use with_alloc::RingBuffer;
+
+#[cfg(feature = "const_generics")]
+mod with_const_generics;
+
 
 mod with_generic_array;
 pub use generic_array::{typenum, ArrayLength};
