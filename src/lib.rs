55deae1c49c8998f5a897a9542fc84441254b458
jonay2000 <jonabent@gmail.com>
2020-10-03 18:16:07 +0200

    Feature-gated generic-array

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,12 @@ pub use with_alloc::RingBuffer;
 
 #[cfg(feature = "const_generics")]
 mod with_const_generics;
+#[cfg(feature = "const_generics")]
+pub use with_const_generics::RingBuffer as ConstGenericRingBuffer;
 
+#[cfg(feature = "generic_array")]
 mod with_generic_array;
+#[cfg(feature = "generic_array")]
 pub use generic_array::{typenum, ArrayLength};
+#[cfg(feature = "generic_array")]
 pub use with_generic_array::RingBuffer as GenericRingBuffer;
