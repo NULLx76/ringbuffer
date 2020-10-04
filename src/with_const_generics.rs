e5f6869ae3b72b60e7a6a6a97d7c4cd9bac9ea79
jonay2000 <jonabent@gmail.com>
2020-10-04 17:19:48 +0200

    removed new_uninit because it is unsound

--- src/with_const_generics.rs
+++ src/with_const_generics.rs
@@ -52,31 +52,6 @@ impl<T: Default, const CAP: usize> ConstGenericRingBuffer<T, CAP> {
     }
 }
 
-impl<T, const CAP: usize> ConstGenericRingBuffer<T, CAP> {
-    /// Creates a new RingBuffer with uninitialized elements. This is unsafe because this relies on
-    /// creating uninitialized memory.
-    ///
-    /// Still it's recommended to use the [`Self::new`] or [`Default::default`] methods to create a
-    /// RingBuffer, whenever the type T implements default.
-    ///
-    /// # Safety
-    ///
-    /// Using this function is not actually that unsafe, because the ringbuffer makes sure you have
-    /// to write to an unitialized element before you can read it by only moving the index forward
-    /// when you write. Therefore you can't ever accidentally read uninitialized memory.
-    #[inline]
-    #[cfg(feature = "generic_uninit")]
-    pub unsafe fn new_uninit() -> Self {
-        let arr: [T; CAP] = MaybeUninit::uninit().assume_init();
-
-        Self {
-            buf: arr,
-            index: 0,
-            length_counter: 0,
-        }
-    }
-}
-
 impl<T: 'static + Default, const CAP: usize> RingBuffer<T> for ConstGenericRingBuffer<T, CAP> {
     #[inline]
     fn len(&self) -> usize {
@@ -171,31 +146,5 @@ mod tests {
     fn test_index_zero_length() {
         let b = ConstGenericRingBuffer::<i32, 2>::new();
         let _ = b[2];
-    }
-
-    #[test]
-    fn test_uninit() {
-        let mut b = unsafe { ConstGenericRingBuffer::<_, 2>::new_uninit() };
-        assert_eq!(b.peek(), None);
-
-        assert_eq!(b.len(), 0);
-        assert_eq!(b.capacity(), 2);
-
-        b.push(1);
-        b.push(2);
-        b.push(3);
-
-        assert_eq!(b.len(), 2);
-        assert_eq!(b.capacity(), 2);
-
-        assert_eq!(b.get_absolute(0).unwrap(), &3);
-        assert_eq!(b.get_absolute(1).unwrap(), &2);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_uninit_out_of_bounds() {
-        let b = unsafe { ConstGenericRingBuffer::<i32, 2>::new_uninit() };
-        let _ = b[0];
     }
 }
